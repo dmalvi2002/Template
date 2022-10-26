1594b2f4a9e4942b5d514bd01362f484998a3bb6
Abu Bakar Siddique <80147468+dmalvi2002@users.noreply.github.com>
2022-10-26 15:39:07 +0600

    Added codes

---
+++ app.js
@@ -0,0 +1,41 @@
+Vue.createApp({
+  data() {
+    return {
+      // Dummy data
+      id: "id132312",
+      uuid: "uuid13514351",
+      given_name: "Jane",
+      family_name: "Kooper",
+      username: "jane234",
+      email: "dummy@dummy.com",
+      role: {
+        name: "Admin",
+      },
+      status: false, //boolean, Active or Inactive,
+      state: "Online", //string, Online, Away,
+      protected: true, //boolean, On, Off (its for Delete Protection)
+    };
+  },
+  computed: {
+    active_status_styles() {
+      return {
+        ["text-gray-400"]: !this.protected,
+      };
+    },
+    online_status_classes() {
+      if (this.state === "Online") {
+        return {
+          ["bg-green-400"]: true,
+        };
+      } else if (this.state === "Away") {
+        return {
+          ["bg-yellow-400"]: true,
+        };
+      } else {
+        return {
+          ["bg-gray-700"]: true,
+        };
+      }
+    },
+  },
+}).mount("#app");
